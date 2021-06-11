c2f4fc338bea76b398cbbcdd993092585289008f
Thuy Vi <17521254@gm.uit.edu.vn>
2021-06-11 14:56:56 +0700

    commit lan 3

--- src/main/java/demoGithub_maven/MainTest_2.java
+++ src/main/java/demoGithub_maven/MainTest_2.java
@@ -2,7 +2,11 @@ package demoGithub_maven;
 
 public class MainTest_2 {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("My name is Vi!");
+		System.out.println("My name is Vi!");
+		System.out.println("My name is Vi!");
+		System.out.println("My name is Vi!");
+		System.out.println("My name is Vi!");
 
 	}
 
